816e1fa01b6134a50ccf81e9a747e2acb63d906b
mehran vejdani <mehran.vejdanii@gmail.com>
2023-04-07 19:14:09 +0430

    typewrite function

--- script.js
+++ script.js
@@ -6,15 +6,39 @@ let flag = true;
 
 const Url = "https://api.quotable.io/random";
 
+window.addEventListener("load", typeWrite);
+
 function loadQuote() {
-  fetch(Url).then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      alert(response.status);
-    }
-  }).then(data=>{
-    quoteArray(index)=data.content
-  })
+  fetch(Url)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        alert(response.status);
+      }
+    })
+    .then((data) => {
+      quoteArray[index] = data.content;
+    });
 }
 
+function typeWrite() {
+  if (flag) {
+    loadQuote();
+    quoteArray[index] += " ";
+    flag = false;
+  }
+  destination.innerHTML =
+    quoteArray[index].substring(0, textPositions) + `<span> \u25AE <span>`;
+
+  if (textPositions++ != quoteArray[index].length) {
+    setTimeout("typeWrite()", 100);
+  } else {
+    quoteArray[index];
+    setTimeout("typeWrite()", 3000);
+    textPositions = 0;
+    flag = true;
+  }
+}
+
+typeWrite();
