6230feebafad1082bef7dbdd9a2a321e4d6cb026
mehran vejdani <mehran.vejdanii@gmail.com>
2023-04-07 19:00:17 +0430

    create loadQuote function

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ let quoteArray = [];
 let index = 0;
 let textPositions = 0;
 let flag = true;
+
+const Url = "https://api.quotable.io/random";
+
+function loadQuote() {
+  fetch(Url).then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      alert(response.status);
+    }
+  }).then(data=>{
+    quoteArray(index)=data.content
+  })
+}
+
